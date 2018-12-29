109c877a7e7228bc9e00a1fa442cd1b79479d8b7
FlamingQaz <flamingqaz@gmail.com>
2018-12-29 18:21:57 -0500

    animation
    started working on the demo animation stuff

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    public Animator playerAnim;
+    public int facing;
+    public float attackTime;
+    public float attackDuration;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      playerAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if ((Input.GetAxis("Vertical")) > 0) { //moving up
+          facing = 1;
+      }
+
+      if ((Input.GetAxis("Vertical")) < 0) { //moving down
+          facing = 2;
+      }
+
+      if ((Input.GetAxis("Horizontal")) < 0) { //moving left
+          facing = 3;
+      }
+
+      if ((Input.GetAxis("Horizontal")) > 0) { //moving right
+          facing = 4;
+      }
+
+      if (Input.GetButton("Fire1")&&Time.time > attackTime) {
+  			   attackTime = Time.time + attackDuration;
+           playerAnim.SetTrigger("attack");
+      }
+
+      playerAnim.SetInteger("facing", facing);
+
+    }
+}
