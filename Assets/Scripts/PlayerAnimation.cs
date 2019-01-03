234f31e87e80ddc6b221f9dd02411e9071911e70
FlamingQaz <flamingqaz@gmail.com>
2019-01-02 20:37:00 -0500

    mep help

--- Assets/Scripts/PlayerAnimation.cs
+++ Assets/Scripts/PlayerAnimation.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAnimation : MonoBehaviour
+public class playerAnimation : MonoBehaviour
 {
     public Animator playerAnim;
     public int facing;
@@ -19,26 +19,35 @@ public class PlayerAnimation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      if ((Input.GetAxis("Vertical")) > 0) { //moving up
-          facing = 1;
-      }
+        if (Input.GetKeyDown("space")) {
+            Debug.Log(Camera.main.ScreenToViewportPoint(Input.mousePosition));
+        }
+        if (Input.GetButtonDown("Fire1")) {
+            if (((Camera.main.ScreenToViewportPoint(Input.mousePosition).y) > 0) && (Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).x - 0.5f) < Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).y - 0.5f))) { //moving up
+                facing = 1;
+                Debug.Log((Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).x - 0.5f) + "<" + Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).y - 0.5f)));
 
-      if ((Input.GetAxis("Vertical")) < 0) { //moving down
-          facing = 2;
-      }
+            }
 
-      if ((Input.GetAxis("Horizontal")) < 0) { //moving left
-          facing = 3;
-      }
+            if (((Camera.main.ScreenToViewportPoint(Input.mousePosition).y) < 0) && (Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).x - 0.5f) > Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).y - 0.5f))) { //moving down
+                Debug.Log((Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).x - 0.5f) + ">" + Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).y - 0.5f)));
+                facing = 2;
+            }
 
-      if ((Input.GetAxis("Horizontal")) > 0) { //moving right
-          facing = 4;
-      }
+            if (((Camera.main.ScreenToViewportPoint(Input.mousePosition).x) < 0) && (Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).x - 0.5f) < Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).y - 0.5f))) { //moving left
+                Debug.Log((Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).x - 0.5f) + "<" + Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).y - 0.5f)));
+                facing = 3;
+            }
 
-      if (Input.GetButton("Fire1")&&Time.time > attackTime) {
-  			   attackTime = Time.time + attackDuration;
-           playerAnim.SetTrigger("attack");
-      }
+            if (((Camera.main.ScreenToViewportPoint(Input.mousePosition).x) > 0) && (Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).x - 0.5f) > Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).y - 0.5f))) { //moving right
+                Debug.Log((Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).x - 0.5f) + ">" + Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).y - 0.5f)));
+                facing = 4;
+            }
+        }
+        if (Input.GetButton("Fire2")&&Time.time > attackTime) {
+  	        attackTime = Time.time + attackDuration;
+            playerAnim.SetTrigger("attack");
+        }
 
       playerAnim.SetInteger("facing", facing);
 
