6e7e3c7d99a6d8f871d68b6d8a23af535e162a49
FlamingQaz <flamingqaz@gmail.com>
2019-01-03 18:59:23 -0500

    fixed turn bug thing

--- Assets/Scripts/PlayerAnimation.cs
+++ Assets/Scripts/PlayerAnimation.cs
@@ -8,12 +8,13 @@ public class playerAnimation : MonoBehaviour
     public int facing;
     public float attackTime;
     public float attackDuration;
-
+    playerMovement playerMovement;
 
     // Start is called before the first frame update
     void Start()
     {
-      playerAnim = GetComponent<Animator>();
+        playerAnim = GetComponent<Animator>();
+        playerMovement = GetComponent<playerMovement>();
     }
 
     // Update is called once per frame
@@ -22,28 +23,7 @@ public class playerAnimation : MonoBehaviour
         if (Input.GetKeyDown("space")) {
             Debug.Log(Camera.main.ScreenToViewportPoint(Input.mousePosition));
         }
-        if (Input.GetButtonDown("Fire1")) {
-            if (((Camera.main.ScreenToViewportPoint(Input.mousePosition).y) > 0) && (Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).x - 0.5f) < Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).y - 0.5f))) { //moving up
-                facing = 1;
-                Debug.Log((Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).x - 0.5f) + "<" + Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).y - 0.5f)));
-
-            }
-
-            if (((Camera.main.ScreenToViewportPoint(Input.mousePosition).y) < 0) && (Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).x - 0.5f) > Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).y - 0.5f))) { //moving down
-                Debug.Log((Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).x - 0.5f) + ">" + Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).y - 0.5f)));
-                facing = 2;
-            }
-
-            if (((Camera.main.ScreenToViewportPoint(Input.mousePosition).x) < 0) && (Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).x - 0.5f) < Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).y - 0.5f))) { //moving left
-                Debug.Log((Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).x - 0.5f) + "<" + Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).y - 0.5f)));
-                facing = 3;
-            }
-
-            if (((Camera.main.ScreenToViewportPoint(Input.mousePosition).x) > 0) && (Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).x - 0.5f) > Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).y - 0.5f))) { //moving right
-                Debug.Log((Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).x - 0.5f) + ">" + Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).y - 0.5f)));
-                facing = 4;
-            }
-        }
+        
         if (Input.GetButton("Fire2")&&Time.time > attackTime) {
   	        attackTime = Time.time + attackDuration;
             playerAnim.SetTrigger("attack");
@@ -51,5 +31,30 @@ public class playerAnimation : MonoBehaviour
 
       playerAnim.SetInteger("facing", facing);
 
+    }
+
+    public void MovementAnim()
+    {
+        float slope = playerMovement.endPos.y - playerMovement.startPos.y - (playerMovement.endPos.x - playerMovement.startPos.x);
+
+        if (!((slope >= 1) || (slope >= -1)) && (Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).x - 0.5f) < Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).y - 0.5f)))
+        { //moving up
+            facing = 1;
+        }
+
+        if (((slope >= 1) || (slope >= -1)) && (Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).x - 0.5f) < Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).y - 0.5f)))
+        { //moving down
+            facing = 2;
+        }
+
+        if (((slope >= 1) || (slope >= -1)) && (Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).x - 0.5f) > Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).y - 0.5f)))
+        {   //moving left
+            facing = 3;
+        }
+
+        if (!((slope >= 1) || (slope >= -1)) && (Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).x - 0.5f) > Mathf.Abs(Camera.main.ScreenToViewportPoint(Input.mousePosition).y - 0.5f)))
+        { //moving right
+            facing = 4;
+        }
     }
 }
--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -4,20 +4,25 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-
     public float speed;
+    public Vector2 startPos;
+    public Vector2 endPos;
+    playerAnimation playerAnimation;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        playerAnimation = GetComponent<playerAnimation>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("Fire1")) {
+            startPos = transform.position;
             transform.position = Vector2.Lerp(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), 1);
+            endPos = transform.position;
+            playerAnimation.MovementAnim();
         }
     }
 }
