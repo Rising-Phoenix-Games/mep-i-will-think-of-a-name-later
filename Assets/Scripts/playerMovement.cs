d49575acd5d8f8d86c6cacb859ceea64e693e93c
Anuj Visavale <flamingqaz@gmail.com>
2019-01-14 09:02:59 -0500

    forgot to clean up my script

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -21,21 +21,10 @@ public class playerMovement : MonoBehaviour
         if (Input.GetButton("Fire1")) {
             startPos = transform.position;
             endPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            //movement(startPos, endPos, speed);
             playerAnimation.movementAnim();
 
         }
         transform.position = Vector2.MoveTowards(transform.position, endPos, Time.deltaTime * 5);
 
-    }
-
-    private IEnumerator movement(Vector2 startPos, Vector2 endPos, float speed)
-    {
-        float i = 0;
-        while ((transform.position.x != endPos.x) || (transform.position.y != endPos.y))
-        {
-            i += speed;
-        }
-        yield return new WaitForEndOfFrame();
     }
 }
