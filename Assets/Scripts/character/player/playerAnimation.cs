746cb1f78a20430b65cb7045061786520f62c9f1
Anuj Visavale <flamingqaz@gmail.com>
2019-02-14 14:52:58 +0530

    Revert "Revert "spring cleaning i guess""
    This reverts commit 3d47692af8bed796c864d0364f68de8b168aca9a.

--- Assets/Scripts/character/player/playerAnimation.cs
+++ Assets/Scripts/character/player/playerAnimation.cs
@@ -9,20 +9,54 @@ public class playerAnimation : MonoBehaviour
     public int facing;
     public float attackTime;
     public float attackDuration;
-    playerMovement playerMovement;
     slash slash;
 
     // Start is called before the first frame update
     void Start()
     {
         playerAnim = GetComponent<Animator>();
-        playerMovement = GetComponent<playerMovement>();
         slash = GameObject.FindGameObjectWithTag("playerAttack").GetComponent<slash>();
     }
 
     // Update is called once per frame
     void Update()
-    {   float direction = 0;
+    {
+        if (Input.GetButton("Fire2")&&Time.time > attackTime)
+        {
+  	        attackTime = Time.time + attackDuration;
+            playerAnim.SetTrigger("attack");
+            
+            slash.dealDamage(slash.target);
+        }
+        
+    }
+
+    public void movementAnim(Vector2 startPos, Vector2 endPos)
+    {
+        float slope = (endPos.y - startPos.y) / (endPos.x - startPos.x);
+
+        if (((slope >= 0.5625) || (slope <= -0.5625)) && (endPos.y < 0.5f))
+        {   //moving up
+            //insert joke about moving up in the world here
+            facing = 1;
+        }
+
+        else if (((slope >= 0.5625) || (slope <= -0.5625)) && (endPos.y > 0.5f))
+        {   //moving down
+            facing = 2;
+        }
+
+        if (!((slope >= 0.5625) || (slope <= -0.5625)) && (endPos.x < 0.5f))
+        {   //moving left
+            facing = 3;
+        }
+
+        else if (!((slope >= 0.5625) || (slope <= -0.5625)) && (endPos.x > 0.5f))
+        {   //moving right
+            facing = 4;
+        }
+
+        float direction = 0;
         switch (facing)
         {
             case 1:
@@ -38,43 +72,7 @@ public class playerAnimation : MonoBehaviour
                 direction = 90f;
                 break;
         }
-        GameObject.FindGameObjectWithTag("playerAttack").transform.rotation = Quaternion.Euler(0, 0, direction);
-
-        if (Input.GetButton("Fire2")&&Time.time > attackTime)
-        {
-  	        attackTime = Time.time + attackDuration;
-            playerAnim.SetTrigger("attack");
-            
-            slash.dealDamage(slash.target);
-        }
-        
         playerAnim.SetInteger("facing", facing);
-
-    }
-
-    public void movementAnim()
-    {
-        float slope = (playerMovement.endPos.y - playerMovement.startPos.y) / (playerMovement.endPos.x - playerMovement.startPos.x);
-
-        if (((slope >= 0.5625) || (slope <= -0.5625)) && (playerMovement.endPos.y < 0.5f))
-        {   //moving up
-            //insert joke about moving up in the world here
-            facing = 1;
-        }
-
-        else if (((slope >= 0.5625) || (slope <= -0.5625)) && (playerMovement.endPos.y > 0.5f))
-        {   //moving down
-            facing = 2;
-        }
-
-        if (!((slope >= 0.5625) || (slope <= -0.5625)) && (playerMovement.endPos.x < 0.5f))
-        {   //moving left
-            facing = 3;
-        }
-
-        else if (!((slope >= 0.5625) || (slope <= -0.5625)) && (playerMovement.endPos.x > 0.5f))
-        {   //moving right
-            facing = 4;
-        }
+        GameObject.FindGameObjectWithTag("playerAttack").transform.rotation = Quaternion.Euler(0, 0, direction);
     }
 }
--- Assets/Scripts/character/player/playerMovement.cs
+++ Assets/Scripts/character/player/playerMovement.cs
@@ -21,7 +21,7 @@ public class playerMovement : MonoBehaviour
         if (Input.GetButton("Fire1")) {
             startPos = transform.position;
             endPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            playerAnimation.movementAnim();
+            playerAnimation.movementAnim(startPos, endPos);
 
         }
         transform.position = Vector2.MoveTowards(transform.position, endPos, Time.deltaTime * 5);
