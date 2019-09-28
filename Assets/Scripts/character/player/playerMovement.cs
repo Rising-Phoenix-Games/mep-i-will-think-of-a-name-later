5616d897c2219b06904c5a8206f09d1ebfd2e9ec
Anug Visavoole <flamingqaz@gmail.com>
2019-09-28 14:13:00 -0400

    added keyboard controls

--- Assets/Scripts/character/player/playerMovement.cs
+++ Assets/Scripts/character/player/playerMovement.cs
@@ -4,24 +4,49 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    public float speed;
     Vector2 endPos;
     playerAnimation playerAnimation;
+    Rigidbody2D playerRB;
+    public bool useKeyboardControls;
 
     // Start is called before the first frame update
     void Start()
     {
         playerAnimation = GetComponent<playerAnimation>();
+
+        playerRB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         playerAnimation.movementAnim();
-        if (Input.GetButton("Fire1")) {
-            endPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        }
-        transform.position = Vector2.MoveTowards(transform.position, endPos, Time.deltaTime * 5);
+        if (useKeyboardControls) {
+            Vector2 playerVelocity = playerRB.velocity;
 
+            if ((Input.GetAxis("Horizontal")) < 0) { //moving left
+                //playerVelocity.x = -1 * 5;
+                transform.Translate (Vector2.left * 5 * Time.deltaTime);
+            }
+            if (Input.GetAxis("Horizontal") > 0) { //moving right
+                //playerVelocity.x = 5;
+                transform.Translate(Vector2.right * 5 * Time.deltaTime);
+            }
+            if ((Input.GetAxis("Vertical")) > 0) { //moving up
+                //playerVelocity.y = -1 * 5;
+                transform.Translate (Vector2.up * 5 * Time.deltaTime);
+            }
+            if (Input.GetAxis("Vertical") < 0) { //moving down
+                //playerVelocity.y = 5;
+                transform.Translate(Vector2.down * 5 * Time.deltaTime);
+            }
+        }
+        else
+        {
+            if (Input.GetButton("Fire1")) {
+                endPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                transform.position = Vector2.MoveTowards(transform.position, endPos, Time.deltaTime * 5);
+            }
+        }
     }
 }
